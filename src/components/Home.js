91f8ac89749f0319559715cb4ae917552488f859
MacwithaQ <maqallaf@gmail.com>
2022-04-07 19:30:30 +0300

    add task card

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
 import React from "react";
+import CategoriesIco from "../Icons/CategoriesIco";
 
 const Home = () => {
   return (
     <div>
-      <div className="MainPanel"></div>
+      <div className="MainPanel">
+        <div className="taskCreate">
+          <div className="taskDiv">
+            <a className="taskTitle">Wash Dishes</a>
+          </div>
+          <div className="taskDiv">
+            <a className="taskDescription">
+              Washing dishes manually using a sponge and soap
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
